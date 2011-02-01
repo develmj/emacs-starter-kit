dc955805ffe23b2cf03e63ddffb486778dde293e
Abhijith <abhijithg@gmail.com>
2011-02-01 09:41:33 +0530

    linux c mode as per linus's c coding standard

--- starter-kit-c.el
+++ starter-kit-c.el
@@ -160,4 +160,13 @@ the character typed."
 (defvar c++-tempo-tags nil
   "Tempo tags for C++ mode")
 
+(defun linux-c-mode ()
+  "C mode with adjusted defaults for use with the Linux kernel."
+  (interactive)
+  (c-mode)
+  (c-set-style "K&R")
+  (setq tab-width 8)
+  (setq indent-tabs-mode t)
+  (setq c-basic-offset 8))
+
 (provide 'starter-kit-c)
